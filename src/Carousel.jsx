e2da5d863ac9a864c741509ab55e652e83190871
Tina Guliuzza <tinaguliuzza@Tinas-Laptop.local>
2022-12-22 11:57:01 -0800

    build class component for Carousel to display images of the pets

---
+++ src/Carousel.jsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+// hooks are only for functional components, not class components
+// must return a render function
+// lifecycle methods: componentDidMount, componentDidUpdate, componentWillUnmount, shouldComponentUpdate
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              onClick={this.handleIndexClick}
+              data-index={index}
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
